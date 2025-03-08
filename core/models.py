5c089f8e8368a4adbc871391c29ea27ac7d54aa3
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-03-08 11:21:50 +0545

    Main

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.TextField()
+    update_at = models.DateTimeField(auto_now_add=True)  # Stores in UTC by default
+
+    def push(self): 
+        return timezone.localtime(self.update_at)  
+
+    def __str__(self):
+        return self.title
