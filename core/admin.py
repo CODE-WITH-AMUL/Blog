26ab5eac3aa47fd7bdc26a94ca2a6e66ac0719fa
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-09-22 23:55:00 +0545

    # Fixed all the old problem

--- core/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Blog , Pdf
-
-admin.site.register(Blog)
-admin.site.register(Pdf)
--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import blog, pdf_list
-
-urlpatterns = [
-    path('', blog, name='blog'),
-    path('pdf/', pdf_list, name='pdf_list')  # Fixed name (removed extra space)
-]
